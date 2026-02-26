e95d9b7439c8f93982a28d601b9492b915258ba4
Amar Karthick <karthickpamar@gmail.com>
2026-02-26 08:16:55 +0000

    made child table as standard

---
+++ qpic/qpic/doctype/costing_sheet_thread_and_ink/costing_sheet_thread_and_ink.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026, TEAMPRO and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class CostingSheetThreadandInk(Document):
+	pass
